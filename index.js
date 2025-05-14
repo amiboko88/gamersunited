676128c0e1d3a3262cdf4384ed9dbc6157d3c4bb
amiboko <amiboko@gmail.com>
2025-05-15 01:12:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const { handleVoiceStateUpdate } = require('./handlers/voiceHandler');
+const { trackGamePresence, validatePresenceOnReady } = require('./handlers/presenceTracker');
+
 
 const client = new Client({
   intents: [
@@ -19,6 +21,11 @@ const { startCleanupScheduler } = require('./handlers/channelCleaner');
 client.once('ready', () => {
   console.log(`שימי הבוט באוויר! ${client.user.tag}`);
   startCleanupScheduler(client); // ← כאן אנחנו מחברים את מנגנון הניקוי
+  validatePresenceOnReady(client); // ← בודק נוכחות גם כשעולה
+});
+
+client.on('presenceUpdate', (_, newPresence) => {
+  trackGamePresence(newPresence);
 });
 
 client.on('voiceStateUpdate', (oldState, newState) => {

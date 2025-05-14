95df8564dbc041bfc50a121e2447df0577140090
amiboko <amiboko@gmail.com>
2025-05-14 09:01:18 +0300

    First take

---
+++ main.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.MessageContent
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`שימי הבוט באוויר! ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);

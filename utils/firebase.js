a590cf12f1e7f403094aa106d839794686dc33f4
amiboko <amiboko@gmail.com>
2025-05-16 01:44:56 +0300

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,7 +1,7 @@
 // firebase.js
 const admin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIAL);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
